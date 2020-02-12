2894873cfc91757bcc0335b8a6aaa1ebacc97acd
Fratee <43710005+Fratee@users.noreply.github.com>
2020-02-12 13:27:23 +0100

    feature/1523
    changed airport level to easy.
    completed airways, added som waypoints, corrected invisible waypoints to visible, removed ils on rwy 04L, sid and star available for both rwy 04L/04R and 22L/22R, ALBET1R was missing INDOD, minor correction on sid/star restriction, spawn pattern now contains airways, added "/long" to long haul airlines in spawn patterns, minor syntax.

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -365,7 +365,7 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
         // },
         {
             icao: 'lipz',
-            level: 'medium',
+            level: 'easy',
             name: 'Venice Marco Polo Airport',
             premium: false
         },
